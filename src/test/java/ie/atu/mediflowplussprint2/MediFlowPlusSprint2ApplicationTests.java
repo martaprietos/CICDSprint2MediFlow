10cb0a803e676dd8d8e13427c041975a4719b9c9
MartaPrieto <G00431347@atu.ie>
2025-11-11 09:27:28 +0000

    Initial Commit

---
+++ src/test/java/ie/atu/mediflowplussprint2/MediFlowPlusSprint2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.mediflowplussprint2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediFlowPlusSprint2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
