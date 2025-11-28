0493d684dc2d9ec3c6780680b44d79e4fdbd304a
Marta Prieto <G00431347@atu.ie>
2025-11-28 09:42:43 +0000

    changed patientID to user and updated related methods and tests

--- src/test/java/ie/atu/mediflowplussprint2/service/PatientServiceTest.java
+++ src/test/java/ie/atu/mediflowplussprint2/service/PatientServiceTest.java
@@ -25,32 +25,32 @@ public class PatientServiceTest {
     void createThenFindById() {
         Patient p = new Patient("P1", "Paul", "paul@atu.ie", "02-02-2000");
         when(repo.save(p)).thenReturn(p); //when save called, return patient
-        when(repo.findByPatientId("P1")).thenReturn(Optional.of(p));//mock that when id is found, return patient information
+        when(repo.findByUsername("P1")).thenReturn(Optional.of(p));//mock that when id is found, return patient information
         service.create(p);
         //test that patient info is actually returned
-        Patient found = service.findByPatientId("P1");
+        Patient found = service.findByUsername("P1");
         assertEquals("Paul", found.getName());
     }
 
     @Test
     void findByIdFail() {
-        when(repo.findByPatientId("P1")).thenReturn(Optional.empty());//pretend to return empty when this is called
+        when(repo.findByUsername("P1")).thenReturn(Optional.empty());//pretend to return empty when this is called
         assertThrows(IllegalArgumentException.class, //if empty then throw error when findBy is called
-                () -> service.findByPatientId("P1"));//generate arg exc
+                () -> service.findByUsername("P1"));//generate arg exc
     }
 
     @Test
     void deleteSuccess() {
         Patient p = new Patient("P1", "Bob", "bob@atu.ie", "02-20-2000");
-        when(repo.findByPatientId("P1")).thenReturn(Optional.of(p));
+        when(repo.findByUsername("P1")).thenReturn(Optional.of(p));
         Patient deleted = service.delete("P1");
         verify(repo).delete(p);//make sure delete was called
-        assertEquals("P1", deleted.getPatientId());
+        assertEquals("P1", deleted.getUsername());
     }
 
     @Test
     void deleteFail() {
-        when(repo.findByPatientId("P1")).thenReturn(Optional.empty());
+        when(repo.findByUsername("P1")).thenReturn(Optional.empty());
         assertThrows(IllegalArgumentException.class,
                 () -> service.delete("P1"));
     }
@@ -59,7 +59,7 @@ public class PatientServiceTest {
     void updateEmailSuccess() {
         Patient p = new Patient("P1", "Bob", "bob@atu.ie", "02-02-2000");
         //mock finding and saving
-        when(repo.findByPatientId("P1")).thenReturn(Optional.of(p));
+        when(repo.findByUsername("P1")).thenReturn(Optional.of(p));
         when(repo.save(p)).thenReturn(p);//store then return
         Optional<Patient> updatedOpt = service.updateEmail("P1", "new@atu.ie");
         assertTrue(updatedOpt.isPresent());
@@ -68,7 +68,7 @@ public class PatientServiceTest {
 
     @Test
     void updateEmailFail() {
-        when(repo.findByPatientId("P1")).thenReturn(Optional.empty());
+        when(repo.findByUsername("P1")).thenReturn(Optional.empty());
         assertThrows(IllegalArgumentException.class,
                 () -> service.updateEmail("P1", "new@atu.ie"));
     }
@@ -76,7 +76,7 @@ public class PatientServiceTest {
     @Test
     void updateNameSuccess() {
         Patient p = new Patient("P1", "Mike", "mike@atu.ie", "02-02-2000");
-        when(repo.findByPatientId("P1")).thenReturn(Optional.of(p));
+        when(repo.findByUsername("P1")).thenReturn(Optional.of(p));
         when(repo.save(p)).thenReturn(p);
         Optional<Patient> updated = service.updateName("P1", "NewName");
         assertTrue(updated.isPresent());
@@ -86,7 +86,7 @@ public class PatientServiceTest {
     @Test
     void updateDOBSuccess() {
         Patient p = new Patient("P1", "Mike", "mike@atu.ie", "20-20-2000");
-        when(repo.findByPatientId("P1")).thenReturn(Optional.of(p));
+        when(repo.findByUsername("P1")).thenReturn(Optional.of(p));
         when(repo.save(p)).thenReturn(p);
         Optional<Patient> updated = service.updateDOB("P1", "20-02-1999");
         assertTrue(updated.isPresent());
